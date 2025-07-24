9a90b579d40dc4d2bb4c9f2a57fca314a67faace
e-mandy <andymfrd02@gmail.com>
2025-07-24 22:08:03 +0100

    Dynamisation de la page (1/2)

--- src/app/components/contact-component/contact-component.ts
+++ src/app/components/contact-component/contact-component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Adress } from '../../interfaces/adress';
 
 @Component({
   selector: 'app-contact-component',
@@ -7,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrl: './contact-component.css'
 })
 export class ContactComponent {
-
+  adresses: Adress[] = [
+    {
+       type: "Adress",
+       icon: "fas fa-phone text-xs",
+       description: "123 Demo Avenue, New York, NY 10160, United States"
+    },
+    {
+       type: "Email",
+       icon: "fas fa-envelope text-xs",
+       description: "mail@example.com"
+    },
+    {
+       type: "Adress",
+       icon: "fas fa-phone text-xs",
+       description: "+1 123-456-7890"
+    }
+  ]
 }
