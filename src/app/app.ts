8ce33dbf4677581189f90925ed9a4eb759695f1a
wesley-kami <okwuwesley@gmail.com>
2025-07-25 00:37:42 +0100

    Dohi

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './components/navbar/navbar';
 import { LessonCard } from './components/lesson-card/lesson-card';
+import { HomeComponent } from './components/home-component/home-component';
 
 
 @Component({
   selector: 'app-root',
-  imports: [Navbar, LessonCard],
+  imports: [Navbar, LessonCard,HomeComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
