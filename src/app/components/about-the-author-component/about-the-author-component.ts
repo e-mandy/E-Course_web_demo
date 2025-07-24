f80611a0b6852730dc14165b1733e4981a3ba605
mouwaficbdr <badaroumouwafic@gmail.com>
2025-07-24 18:47:50 +0100

    Ajout du composant AboutTheAuthor avec ses fichiers HTML, CSS et de test

---
+++ src/app/components/about-the-author-component/about-the-author-component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about-the-author-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './about-the-author-component.html',
+  styleUrl: './about-the-author-component.css'
+})
+export class AboutTheAuthorComponent {
+
+}
