680d0a667c17ce42568c45a91179f30df1ba86b8
e-mandy <andymfrd02@gmail.com>
2025-07-24 18:28:13 +0100

    Mise en place du routing (1/2)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home-component/home-component';
+import { AboutComponent } from './components/about-component/about-component';
+import { LessonsComponent } from './components/lessons-component/lessons-component';
+import { ContactComponent } from './components/contact-component/contact-component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+    path: '',
+    component: HomeComponent
+  },
+
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+
+  {
+    path: 'lessons',
+    component: LessonsComponent
+  },
+
+  {
+    path: 'contact',
+    component: ContactComponent
+  }
+];
---
+++ src/app/components/lessons-component/lessons-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lessons-component',
+  imports: [],
+  templateUrl: './lessons-component.html',
+  styleUrl: './lessons-component.css'
+})
+export class LessonsComponent {
+
+}
