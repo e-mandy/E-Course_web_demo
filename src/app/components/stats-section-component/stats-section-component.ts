e5dd6669ee8dd0fb0405f303b51263f5d56cfd8f
mouwaficbdr <badaroumouwafic@gmail.com>
2025-07-24 18:49:13 +0100

    Ajout du composant StatsSection avec ses fichiers HTML, CSS et de test

---
+++ src/app/components/stats-section-component/stats-section-component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stats-section-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './stats-section-component.html',
+  styleUrl: './stats-section-component.css'
+})
+export class StatsSectionComponent {
+
+}
